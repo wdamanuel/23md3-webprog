7d84c1f9bbbc42b423e3ddce3dabc75b23ae2819
Manuel Haigermoser <manuel.haigermoser@wda-innsbruck.at>
2023-11-21 15:16:33 +0100

    add color-flipper.

---
+++ 231121 - JS Basics/color-flipper/script.js
@@ -0,0 +1,15 @@
+"use strict";
+
+const colors = ["green", "red", "rgba(133,122,200,0.5)", "#f15025"];
+const $btn = document.querySelector('#btn');
+const color = document.querySelector('.color');
+
+$btn.addEventListener('click', function(){
+  const randomNumber = getRandomNumber();
+    document.body.style.backgroundColor = colors[randomNumber];
+    color.textContent = colors[randomNumber];
+});
+
+function getRandomNumber() {
+  return Math.floor(Math.random() * colors.length);
+}
